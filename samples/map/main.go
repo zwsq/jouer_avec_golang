e1dddfbaec6412e446b8bd8eceff0a1194d1f961
zwsq <abolemail@gmail.com>
2025-03-03 06:06:00 +0330

    added more stuffs to map

--- samples/map/main.go
+++ samples/map/main.go
@@ -6,14 +6,40 @@ func main() {
 	iMap := map[string]string{"home_range": "192.168.2.0/24", "company_range": "192.168.1.0/24", "lab_range": "192.168.102.0/24"}
 	nMap := make(map[int]int)
 
-	// Looping over map
+	// Looping over map keys and values
 	for k, v := range iMap {
 		fmt.Printf("K: %v\tV: %v\n", k, v)
 	}
 
-	for i := 1; i <= 10; i++ {
-		nMap[i] = 10 ^ i
+	// Looping over map keys
+	fmt.Println("-----------------------------------------------------")
+	for k := range iMap {
+		fmt.Println(k)
 	}
 
+	// Looping over map values
+	fmt.Println("-----------------------------------------------------")
+	for _, k := range iMap {
+		fmt.Println(k)
+	}
+
+	// Assigning a values to maps
+	fmt.Println("-----------------------------------------------------")
+	for i := 1; i <= 10; i++ {
+		nMap[i] = 10 * i
+	}
 	fmt.Println(nMap)
+
+	// Deleting item from map
+	fmt.Println("-----------------------------------------------------")
+	delete(iMap, "company_range") // if the key does not exist, the compiler won't panic.
+	fmt.Println(iMap)
+
+	// Making sure the item really exists since the default value would return in case of
+	// accessing a key which does not exist
+	if v, ok := iMap["Akbar"]; !ok {
+		fmt.Println("Queried value does not exist.")
+	} else {
+		fmt.Printf("exits %v\n", v)
+	}
 }
