552030b61dde73aa9f7b16dbef4b289d4fc1ab82
zwsq <abolemail@gmail.com>
2025-03-08 05:46:31 +0330

    a simple exaple of interface added.

---
+++ samples/interface/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+}
+
+type secretAgent struct {
+	person person
+	ltk    bool
+}
+
+type human interface {
+	sayYourName()
+}
+
+func (p person) sayYourName() {
+	fmt.Println("My name is", p.name)
+}
+
+func (sa secretAgent) sayYourName() {
+	fmt.Println("My name is", sa.person.name, "and I'm a secrent agent.")
+}
+
+func sayIt(h human) {
+	h.sayYourName()
+}
+func main() {
+	zwsq := person{
+		name: "Abolfazl",
+	}
+	james := secretAgent{
+		person: person{
+			name: "James",
+		},
+		ltk: true,
+	}
+	sayIt(zwsq)
+	sayIt(james)
+}
