ff6429a9abfbc849a4adf05884c805f28cbc43d9
zwsq <abolemail@gmail.com>
2025-03-09 07:02:35 +0330

    More interface exercises

--- samples/interface/main.go
+++ samples/interface/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 type person struct {
 	name string
@@ -26,16 +30,53 @@ func (sa secretAgent) sayYourName() {
 func sayIt(h human) {
 	h.sayYourName()
 }
+
+// ----------------------------------------------------------------------
+type book struct {
+	title string
+}
+type count int
+
+func (b book) String() (selfIntroduction string) {
+	return fmt.Sprint("I'm a book and my name is ", b.title)
+}
+
+func (c count) String() (howManyIAm string) {
+	return fmt.Sprint("I have ", strconv.Itoa(int(c)))
+}
+
+func customeLogger(s fmt.Stringer) {
+	log.Println("Log from svc", s.String())
+}
+
+// ----------------------------------------------------------------------
+
 func main() {
-	zwsq := person{
-		name: "Abolfazl",
+	{
+		// zwsq := person{
+		// 	name: "Abolfazl",
+		// }
+		// james := secretAgent{
+		// 	person: person{
+		// 		name: "James",
+		// 	},
+		// 	ltk: true,
+		// }
+		// sayIt(zwsq)
+		// sayIt(james)
 	}
-	james := secretAgent{
-		person: person{
-			name: "James",
-		},
-		ltk: true,
+	{
+		learnGo := book{
+			title: "Learn Go in a week!",
+		}
+
+		var age count = 33
+
+		fmt.Println(learnGo)
+		fmt.Println(age)
+		customeLogger(age)
 	}
-	sayIt(zwsq)
-	sayIt(james)
+	{
+
+	}
 }
